43131bec17f9093cfa027737bf86f2b10ead5b88
Ece Sefercioğlu <seferciogluecce@users.noreply.github.com>
2017-11-18 00:40:53 +0300

    Web site system files are initiazed. Web site is establised. It is connected to databese via Class Library.

---
+++ WebSite/App_Code/BasePage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GgDB;
+
+/// <summary>
+/// Summary description for BasePage
+/// </summary>
+public class BasePage : System.Web.UI.Page
+{
+    private GgDBEntities field;
+
+    public GgDBEntities GgDbProvider
+    {
+        get 
+        {
+            if (field == null)
+                field = new GgDBEntities();
+            return field; 
+        }
+    }
+    //private List<Product> list;
+
+    //public List<Product> Liste
+    //{
+    //    get 
+    //    {
+    //        if (list == null)
+    //            list = new List<Product>();
+    //        return list;
+    //    }
+    //}
+    
+
+
+}
