d89e36130d5dc68ace4610f14671c33d7afc7503
Oğuzhan Can <oguzhan95@gmail.com>
2017-11-21 16:02:35 +0300

    added Supplier class

---
+++ ClassLibrary/Partial/Supplier.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GgDB
+{
+    public partial class Supplier
+    {
+
+    }
+    public partial class GgDBEntities
+    {
+        public List<Supplier> GetSupplierList()
+        {
+            return Supplier.Where(i => i.Deleted == false).ToList();
+        }
+
+        public void AddSupplier(int userID, int districtID, string companyName, string adress, string phone, string photoPath)
+        {
+            Supplier s = new Supplier();
+            s.Adress = adress;
+            s.CompanyName = companyName;
+            s.CompanyPhoto = photoPath;
+            s.DistrictID = districtID;
+            s.UserID = userID;
+            s.Phone = phone;
+            AddToSupplier(s);
+            SaveChanges();
+        }
+    }
+}
