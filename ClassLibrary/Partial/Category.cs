9ed7f11ee882a3b78db8e31686131f23d0a4d1a7
Ece Sefercioğlu <seferciogluecce@users.noreply.github.com>
2017-11-26 23:42:10 +0300

    Added Category and District files

---
+++ ClassLibrary/Partial/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GgDB
+{
+    public partial class Category
+    {
+        
+    }
+    public partial class GgDBEntities
+    {
+        public List<Category> GetCategoryList()
+        {
+            return Category.Where(i => i.Deleted == false).ToList();
+        }
+    }
+}
---
+++ ClassLibrary/Partial/District.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GgDB
+{
+    public partial class District
+    {
+
+    }
+    public partial class GgDBEntities
+    {
+        District d = new District();
+        public List<District> GetDistrictList()
+        {
+            return District.Where(i => i.Deleted == false).ToList();
+        }
+    }
+}
