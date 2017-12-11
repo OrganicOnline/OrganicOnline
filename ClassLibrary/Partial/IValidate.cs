d5ae920b22a9f6620f632d230daf2a709382e8ca
Elif Ak <elifak.35@gmail.com>
2017-12-11 22:42:53 +0300

    Add new class for validation

---
+++ ClassLibrary/Partial/IValidate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Objects;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GgDB
+{
+    public enum ChangeAction
+    {
+        Insert,
+        Update,
+        Delete
+    }
+
+    public interface IValidate
+    {
+        void Validate(ChangeAction action);
+    }
+}
