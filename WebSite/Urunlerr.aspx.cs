52104de1b4428542edf266a5de23ca116fdc53cc
Gülçin Baykal <gulcinbaykal@hotmail.com>
2017-12-11 22:20:11 +0300

    added new functionalities

--- WebSite/Urunlerr.aspx.cs
+++ WebSite/Urunlerr.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GgDB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,20 +13,106 @@ public partial class Urunlerr : BasePage
         if (IsPostBack) return;
         dlUrunler.DataSource = GgDbProvider.GetProductList();
         dlUrunler.DataBind();
+        if (dlUrunler.Items.Count == 0)
+        {
+            pnlUrunler.Visible = false;
+            pnlBilgi.Visible = true;
+        }
+        else
+        {
+            pnlUrunler.Visible = true;
+            pnlBilgi.Visible = false;
+        }
 
         lblTopFiyat.Text = "0 TL";
         lblUrunSayisi.Text = "0 Ürün";
         btnSiparis.CssClass += "btn btn-default butonn disabled";
-       
-    }
-    protected void dlUrunler_ItemCommand(object source, DataListCommandEventArgs e)
-    {
 
-        if (e.CommandName == "guncelle")
+
+        ddlFiltering.DataSource = GgDbProvider.GetCategoryList();
+        ddlFiltering.DataBind();
+
+        lstDistrict.DataSource = GgDbProvider.GetDistrictList();
+        lstDistrict.DataBind();
+        lstDistrict.Items.Add(new ListItem() { Text = "Seçiniz" });
+
+
+
+
+        List<double> toplamFiyat = new List<double>();
+        if (Session["ToplamFiyat"] != null)
         {
-            Response.Write(e.CommandArgument.ToString());
+            toplamFiyat = Session["ToplamFiyat"] as List<double>;
+            double toplam = ToplamUrunFiyatiniHesapla(toplamFiyat);
+            lblTopFiyat.Text = toplam.ToString();
+
+            lblSayi.Text = Session["ToplamUrunSayisi"].ToString();
+            lblUrunSayisi.Text = Session["ToplamUrunSayisi"].ToString();
+            lblUrunSayisi.Text += " " + "Ürün";
+
+            lblTopFiyat.Text += " " + "TL";
+            btnSiparis.CssClass += "btn btn-default butonn";
+        }
+
+        else
+        {
+            lblTopFiyat.Text = "";
+
+            lblSayi.Text = "0";
+            lblUrunSayisi.Text = "Sepetinizde ürün yok.";
+
         }
     }
+    protected void dlUrunler_ItemCommand(object source, DataListCommandEventArgs args)
+    {
+
+        if (args.CommandName == "btnLeaveComment")
+        {
+            HiddenField hfCommentedProductId = (HiddenField)args.Item.FindControl("hfCommentedProductId");
+            TextBox txtCommentBody = (TextBox)args.Item.FindControl("txtCommentBody");
+            TextBox txtCommentHeader = (TextBox)args.Item.FindControl("txtCommentHeader");
+            DropDownList ddlStar = (DropDownList)args.Item.FindControl("ddlStar");
+
+            string comment = txtCommentBody.Text;
+            string header = txtCommentHeader.Text;
+            int star = Convert.ToInt32(ddlStar.SelectedValue);
+
+
+            if (Session["Oturum"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+            else
+            {
+                User autUser = Session["Oturum"] as User;
+                if (autUser == default(User))
+                    Response.Redirect("Login.aspx");
+                else
+                {
+                    GgDbProvider.AddComment(header, comment, autUser.ObjectID, Convert.ToInt32(hfCommentedProductId.Value), star);
+                    //pnlInfo.Visible = true;
+                    Response.Write("<script>alert('Yorumunuz başarıyla eklendi. ');</script>");
+                }
+            }
+
+
+        }
+    }
+
+    protected void dlUrunler_ItemDataBound(object sender, DataListItemEventArgs args)
+    {
+        if (args.Item.ItemType == ListItemType.Item || args.Item.ItemType == ListItemType.AlternatingItem)
+        {
+            Repeater childRepeater = (Repeater)args.Item.FindControl("rptCommentList");
+            HiddenField hfCommentedProductId = (HiddenField)args.Item.FindControl("hfCommentedProductId");
+            childRepeater.DataSource = GgDbProvider.GetCommentListForProduct(Convert.ToInt32(hfCommentedProductId.Value)).ToList();
+            childRepeater.DataBind();
+
+        }
+    }
+
+
+
     protected void btnSepeteEkle_Click(object sender, EventArgs e)
     {
         btnSiparis.CssClass += "btn btn-default butonn";
@@ -34,6 +121,8 @@ public partial class Urunlerr : BasePage
         if (Session["Urunler"] != null)
         {
             urunler = Session["Urunler"] as List<string>;
+            if (urunler == null)
+                urunler = new List<string>();
             string eklenenUrun = lblUrunID.Value.ToString();
             urunler.Add(eklenenUrun);
             Session["Urunler"] = urunler;
@@ -64,7 +153,7 @@ public partial class Urunlerr : BasePage
         }
 
 
-        double fiyat = Convert.ToDouble(hfToplamFiyat.Value);
+        double fiyat = Convert.ToDouble(hfToplamFiyat.Value.Replace(".", ","));
         List<double> toplamFiyat = new List<double>();
 
         //if (Session["ToplamFiyat"] != null)
@@ -99,7 +188,7 @@ public partial class Urunlerr : BasePage
         txtMiktar.Text = "";
 
     }
-    protected double ToplamUrunFiyatiniHesapla(List<double> toplamFiyat)
+    public double ToplamUrunFiyatiniHesapla(List<double> toplamFiyat)
     {
         double x = 0;
         foreach (double item in toplamFiyat)
@@ -109,9 +198,11 @@ public partial class Urunlerr : BasePage
         return x;
     }
 
-    protected bool FarkliBirManavMi(List<string> urunler, string eklenenUrun)
+    public bool FarkliBirManavMi(List<string> urunler, string eklenenUrun)
     {
         int lastItem = urunler.Count - 1;
+        if (lastItem - 1 < 0)
+            return false;
         int karsılastırılacakUrun = Convert.ToInt32(urunler[lastItem - 1]);
         int eklenenUrunId = Convert.ToInt32(eklenenUrun);
         GgDB.Product p_1 = new GgDB.Product();
@@ -121,7 +212,7 @@ public partial class Urunlerr : BasePage
         p_2 = GgDbProvider.Product.First(i => i.ObjectID == eklenenUrunId);
 
 
-        if(p_1.Supplier.ObjectID == p_2.Supplier.ObjectID)
+        if (p_1.Supplier.ObjectID == p_2.Supplier.ObjectID)
         {
             return false;
         }
@@ -134,4 +225,81 @@ public partial class Urunlerr : BasePage
     {
         Response.Redirect("UrunSiparis.aspx");
     }
+
+    protected void ddlFiltering_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (ddlFiltering.SelectedValue == "Seçiniz")
+            dlUrunler.DataSource = GgDbProvider.GetProductList();
+        else
+            dlUrunler.DataSource = GgDbProvider.GetProductList().Where(i => i.CategoryID == Convert.ToInt32(ddlFiltering.SelectedValue));
+        dlUrunler.DataBind();
+        if (dlUrunler.Items.Count == 0)
+        {
+            pnlUrunler.Visible = false;
+            pnlBilgi.Visible = true;
+        }
+        else
+        {
+            pnlUrunler.Visible = true;
+            pnlBilgi.Visible = false;
+        }
+    }
+
+    protected void ddlSorting_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (ddlSorting.SelectedValue == "artan")
+        {
+            dlUrunler.DataSource = GgDbProvider.GetProductList().OrderBy(i => i.Price);
+            dlUrunler.DataBind();
+        }
+        else if (ddlSorting.SelectedValue == "azalan")
+        {
+            dlUrunler.DataSource = GgDbProvider.GetProductList().OrderByDescending(i => i.Price);
+            dlUrunler.DataBind();
+        }
+        if (dlUrunler.Items.Count == 0)
+        {
+            pnlUrunler.Visible = false;
+            pnlBilgi.Visible = true;
+        }
+        else
+        {
+            pnlUrunler.Visible = true;
+            pnlBilgi.Visible = false;
+        }
+    }
+
+    protected void btnSearch_Click(object sender, EventArgs e)
+    {
+        List<Product> result = new List<Product>();
+        if (txtSearchProduct.Text.Trim() != "" && txtSearchSupplier.Text.Trim() != "")
+            result = GgDbProvider.GetProductList().Where(i => i.ProductName.ToLower().Contains(txtSearchProduct.Text.Trim().ToLower()) || i.Supplier.CompanyName.ToLower().Contains(txtSearchSupplier.Text.Trim().ToLower())).ToList();
+        else if (txtSearchProduct.Text.Trim() != "" && txtSearchSupplier.Text.Trim() == "")
+            result = GgDbProvider.GetProductList().Where(i => i.ProductName.ToLower().Contains(txtSearchProduct.Text.Trim().ToLower())).ToList();
+        else if (txtSearchProduct.Text.Trim() == "" && txtSearchSupplier.Text.Trim() != "")
+            result = GgDbProvider.GetProductList().Where(i => i.Supplier.CompanyName.ToLower().Contains(txtSearchSupplier.Text.Trim().ToLower())).ToList();
+        else
+            result = GgDbProvider.GetProductList();
+
+        List<Product> result2 = new List<Product>();
+        foreach (int item in lstDistrict.GetSelectedIndices())
+        {
+            result2.AddRange(result.Where(i => i.Supplier.DistrictID == Convert.ToInt32(lstDistrict.Items[item].Value)).ToList());
+        }
+
+        dlUrunler.DataSource = result2;
+        dlUrunler.DataBind();
+        if (dlUrunler.Items.Count == 0)
+        {
+            pnlUrunler.Visible = false;
+            pnlBilgi.Visible = true;
+        }
+        else
+        {
+            pnlUrunler.Visible = true;
+            pnlBilgi.Visible = false;
+        }
+    }
+
+
 }
